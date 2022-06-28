c3dd9af235ca400bb5a6586486d716376a949a07
Andrey <ilynych0810@gmail.com>
2022-06-28 10:58:12 +0300

    JS: fix, CSS: remove notice

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.header__burger').click(function(event) {
-        $('.header__burger, .header__nav').toggleClass('active')
+        $('.header__burger, .header__nav').toggleClass('active');
+        $('body').toggleClass('lock');
     });
-    $('body').toggleClass('lock')
+    
 });
